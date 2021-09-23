6538e45a854b5c4f2e14186e1cc9c8fe484e4d8b
Niall Barber <niall.barber@actual-experience.com>
2021-09-23 07:59:53 +0100

    Updated copy2clipboard return type

--- src/utils/copy-to-clipboard.ts
+++ src/utils/copy-to-clipboard.ts
@@ -1,8 +1,2 @@
-export function copyToClipboard(text: string = ''): void {
-  const copiedText = document.createElement('textarea');
-  copiedText.innerText = text;
-  document.body.appendChild(copiedText);
-  copiedText.select();
-  document.execCommand('copy');
-  copiedText.remove();
-}
+export const copyToClipboard = (text: string = ''): Promise<void> =>
+  navigator.clipboard.writeText(text);
